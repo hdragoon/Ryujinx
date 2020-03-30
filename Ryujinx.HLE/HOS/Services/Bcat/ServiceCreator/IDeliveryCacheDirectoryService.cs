7dc141bd587cbd4852dea80221454230767aeb34
MattsCreative <gamedev1909@gmail.com>
2020-03-30 15:28:36 -0300

    Proper Stubb

--- Ryujinx.HLE/HOS/Services/Bcat/ServiceCreator/IDeliveryCacheDirectoryService.cs
+++ Ryujinx.HLE/HOS/Services/Bcat/ServiceCreator/IDeliveryCacheDirectoryService.cs
@@ -1,31 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
 using Ryujinx.HLE.HOS.Services.Arp;
 
 namespace Ryujinx.HLE.HOS.Services.Bcat.ServiceCreator
 {
-    class IDeliveryCacheDirectoryService : IpcService
+    internal class IDeliveryCacheDirectoryService : IpcService
     {
-    
-      public IDeliveryCacheDirectoryService(ServiceCtx context, ApplicationLaunchProperty applicationLaunchProperty)
-      {
-      
-      }
-      [Command(0)]
-      public ResultCode Open(serviceCtx Context)
-      {
-            return ResultCode.Success;
-       }
-       
-       [Command(1)]
-       public ResultCode Open(serviceCtx Context)
-      {
-            return ResultCode.Success;
-       }
-      
-      [Command(2)]
-       public ResultCode Open(serviceCtx Context)
-      {
-            return ResultCode.Success;
-       }
+        private ApplicationLaunchProperty applicationLaunchProperty;
+
+        public IDeliveryCacheDirectoryService(ApplicationLaunchProperty applicationLaunchProperty)
+        {
+            this.applicationLaunchProperty = applicationLaunchProperty;
+        }
+
+
+
+    }
+}
